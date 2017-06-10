b6f012867809dd7654fe575541a3fdf6c554ba75
Sergiu Puscas <srg.pscs@gmail.com>
2017-06-10 17:39:15 +0300

    added debug setting to config file

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 from string import ascii_lowercase, ascii_uppercase
 
 config = {
-    'secret_key': "asdf",
+    'secret_key': 'asdf',
     'short_url_size': 6,
+    'max_attempts_until_short_url_size_increases': 3,
+
     'alphabet': ascii_lowercase + ascii_uppercase + '0123456789',
 
     'app': {
         'SQLALCHEMY_DATABASE_URI': "postgresql://localhost/url-shortener",
         'SQLALCHEMY_TRACK_MODIFICATIONS': False,
+        'DEBUG': True,
     }
 }
